66afa9345bd806a457d64da8c4c780e9c360f3b0
George Armando <georgearmando54@gmail.com>
2021-05-22 23:22:46 +0100

    Creates and configures the updateGlobalUserData function

--- semana-javascript-expert04/server/src/controllers/roomsController.js
+++ semana-javascript-expert04/server/src/controllers/roomsController.js
@@ -1,16 +1,47 @@
+import Attendee from "../entities/attendee.js"
 import { constants } from "../util/constants.js"
 
 export default class RoomsController {
-  constructor() { }
+  #users = new Map()
+
+  constructor() {
+    this.rooms = new Map()
+  }
 
   onNewConnection(socket) {
     const { id } = socket
     console.log('Connection stablished with', id)
   }
 
-  joinRoom(socket, data) {
-    console.log('received data', data)
-    socket.emit(constants.event.USER_CONNECTED, data)
+  joinRoom(socket, { user, room }) {
+    const userId = user.id = socket.id
+    const roomId = room.id
+
+    const updatedUserData = this.#updateGlobalUserData(
+      userId,
+      user,
+      roomId
+    )
+
+    console.log({ updatedUserData })
+    socket.emit(constants.event.USER_CONNECTED, updatedUserData)
+  }
+
+  #updateGlobalUserData(userId, userData = {}, roomId = '') {
+    const user = this.#users.get(userId) ?? {}
+    const existingRoom = this.rooms.has(roomId)
+
+    const updatedUserData = new Attendee({
+      ...user,
+      ...userData,
+      roomId,
+      // se for o unico na sala
+      isSpeaker: !existingRoom,
+    })
+
+    this.#users.set(userId, updatedUserData)
+
+    return this.#users.get(userId)
   }
 
   getEvents() {
